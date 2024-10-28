abe978e6ce819c80dd875455f632b5d36d85f562
Hitech <Hitech@DESKTOP-H9A7EBK>
2024-10-28 20:54:56 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1. Вывести в столбик 10 раз слово "Привет";
+
+            //int count = 0;
+            //while (count < 10) 
+            //{
+            //    Console.WriteLine("Привет");
+            //    count++;
+            //}
+            //Console.Read();
+
+            //for (int count = 0; count < 10; count++) 
+            //{
+            //    Console.WriteLine("Привет");
+            //}
+            //Console.Read();
+
+            //int count = 0;
+            //do
+            //{
+            //    Console.WriteLine("Привет");
+            //    count++;
+            //}
+            //while (count < 10);
+            //Console.Read();
+
+            //Задача 2. вывести в столбик первые 10 натуральных чисел;
+
+            //int count = 1;
+            //while (count<=10)
+            //{
+            //    Console.WriteLine(count);
+            //    count++;
+            //}
+            //Console.Read();
+
+            //for (int count = 1; count <= 10; count++) 
+            //{
+            //    Console.WriteLine(count);
+            //}
+            //Console.Read();
+
+            //int count = 1;
+            //do
+            //{
+            //    Console.WriteLine(count);
+            //    count++;
+            //}
+            //while (count <= 10);
+            //Console.Read();
+
+            //Задача 3. вывести в столбик первые 5 натуральных чисел в обратном порядке
+
+            //int count = 5;
+            //while (count >= 1)
+            //{
+            //    Console.WriteLine(count);
+            //    count--;
+            //}
+            //Console.Read();
+
+            //for (int count = 5; count >= 1; count--) 
+            //{
+            //    Console.WriteLine(count);
+            //}
+            //Console.Read();
+
+            //int count = 5;
+            //do
+            //{
+            //    Console.WriteLine(count);
+            //    count--;
+            //}
+            //while (count >= 1);
+            //Console.Read();
+
+            //Задача 4. вывести на экран целые числа из интервала от 1 до n;
+
+            //Console.Write("Введите целове число");
+            //string st = Console.ReadLine();
+            //Console.Write("n=");
+            //int n = int.Parse(Console.ReadLine());
+            //int count = 1;
+            //while (count <= n)
+            //{
+            //    Console.WriteLine(count);
+            //    count++;
+            //}
+            //Console.Read();
+
+            //Console.Write("Введите целое число=");
+            //int n = int.Parse(Console.ReadLine());
+            //for (int count = 1; count <= n; count++)
+            //{
+            //    Console.WriteLine(count);
+            //}
+            //Console.Read();
+
+            //Console.Write("Введите целое число=");
+            //int n = int.Parse(Console.ReadLine());
+            //int count = 1;
+            //do
+            //{
+            //    Console.WriteLine(count);
+            //    count++;
+            //}
+            //while (count <= n);
+            //Console.Read();
+
+           
+        }
+    }
+}
